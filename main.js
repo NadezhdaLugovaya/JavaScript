d09439626907eff71bd0d96ac0236ab1ac6aad13
Nadezhda <lugovaja@icloud.com>
2021-05-28 14:00:54 +0300

    HW1

---
+++ main.js
@@ -0,0 +1,22 @@
+// №1
+let Tc = 25;
+let Tf;
+
+alert (Tf = (9 / 5) * Tc + 32);
+// №2
+let admin;
+let firstName;
+
+firstName = 'Василий';
+admin = firstName;
+alert (admin);
+
+// №3 
+console.log(1000 + "108") // Ответ: 1000108
+
+// №4
+
+// async - загрузает скрипт асинхронно 
+
+// defer - скрипт должен выполняться после разбора документа
+
